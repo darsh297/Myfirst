d41bb820af549c180ea1eefa9eae7fd7614ff82a
darsh297 <20it019@charusat.edu.in>
2022-09-09 18:02:36 +0530

    git intilize .

--- src/com/cc/Main.java
+++ src/com/cc/Main.java
@@ -1,9 +1,35 @@
 package com.cc;
+class myname
+{
+	private String name;
+	private int Age;
+	
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello");
-    }
+	public String getname()
+	{
+		return name;
+	}
+	public void setname(String name)
+	{
+		this.name=name;
+	}
+	public int getAge()
+	{
+		return Age;
+	}
+	public void setAge(int Age)
+	{
+		this.Age=Age;
+	}
 }
+public class Main {
+	public static void main(String[] args)
+	{
+		myname T=new myname();
+		T.setname("Darshan ");
+		T.setAge(20);
+		System.out.println(T.getname());
+		System.out.println(T.getAge());
+	}
+}
+
